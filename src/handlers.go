dbfd8ed6c2322ccdbd5d12e9401c727014356596
Muhammed Ali Sit <masit@outlook.com>
2017-05-31 12:33:56 -0500

    removed an unused handler function

--- src/handlers.go
+++ src/handlers.go
@@ -75,36 +75,6 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "Method is not allowed.", http.StatusBadRequest)
-	}
-
-	var funcMap = template.FuncMap{
-		"titleF": func() string {
-			return "munhasir"
-		},
-	}
-
-	template, err := template.New("index.html").Funcs(funcMap).ParseFiles("templates/index.html")
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	info := make(map[string]string)
-	info["title"] = "munhasir"
-	info["content"] = "A platform to keeping diaries for those who are cautious(or paranoid)."
-
-	err = template.Execute(w, info)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-}
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
